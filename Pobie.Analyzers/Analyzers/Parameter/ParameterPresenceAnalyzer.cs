69b670df24f6c57acc2c7b47dae6c92ce8291e9a
Pierre-Olivier Boulet <poboulet@pm.me>
2025-03-24 16:48:45 -0400

    Initial commit

---
+++ Pobie.Analyzers/Analyzers/Parameter/ParameterPresenceAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Diagnostic = Microsoft.CodeAnalysis.Diagnostic;
+using SemanticModel = Microsoft.CodeAnalysis.SemanticModel;
+
+namespace Analyzers.Analyzers.Parameter;
+
+public abstract class ParameterPresenceAnalyzer : ParameterAnalyzer
+{
+    protected override void AnalyzeInvocation(
+        OperationAnalysisContext context,
+        InvocationExpressionSyntax syntax,
+        SemanticModel semanticModel
+    )
+    {
+        if (HasTargetParameter(syntax, semanticModel))
+        {
+            return;
+        }
+
+        context.ReportDiagnostic(
+            Diagnostic.Create(SupportedDiagnostics.First(), syntax.Expression.GetLocation())
+        );
+    }
+
+    private bool HasTargetParameter(
+        InvocationExpressionSyntax invocationSyntax,
+        SemanticModel semanticModel
+    )
+    {
+        return invocationSyntax.ArgumentList.Arguments.Any(arg =>
+            GetParameterName(semanticModel, arg) == TargetParameterName
+        );
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Game.CSharp.CodeAnalysis
+Contains various Roslyn analyzers useful for C# game development.
