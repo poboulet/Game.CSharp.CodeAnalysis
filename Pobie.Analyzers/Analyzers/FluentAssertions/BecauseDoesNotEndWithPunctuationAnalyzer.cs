3a04e7f7e5e84b474b04607586cd9f0a8fb45051
pobie <poboulet@pm.me>
2025-03-24 17:05:35 -0400

    Added Analyzers for FluentAssertions

---
+++ Pobie.Analyzers/Analyzers/FluentAssertions/BecauseDoesNotEndWithPunctuationAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+using Analyzers.Analyzers.Parameter;
+using Analyzers.Validations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Analyzers.Analyzers.FluentAssertions;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class BecauseDoesNotEndWithPunctuationAnalyzer : ParameterValueAnalyzer
+{
+    public const string Id = "UT0003";
+
+    private static readonly DiagnosticDescriptor DiagnosticDescriptor =
+        new(
+            Id,
+            Resources.UT0003Title,
+            Resources.UT0003MessageFormat,
+            DiagnosticCategory.Usage.ToString(),
+            DiagnosticSeverity.Warning,
+            true,
+            Resources.UT0003Description
+        );
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+        ImmutableArray.Create(Rule);
+
+    protected override string TargetClassName => "BooleanAssertions";
+    protected override string TargetMethodName => "*";
+    protected override string TargetParameterName => "because";
+    protected override DiagnosticDescriptor Rule => DiagnosticDescriptor;
+
+    protected override IValidation Validation { get; } = new StringDoesNotEndWithPunctuation();
+}
---
+++ Pobie.Analyzers/Analyzers/Parameter/ParameterValueAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Analyzers.Validations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Diagnostic = Microsoft.CodeAnalysis.Diagnostic;
+using DiagnosticDescriptor = Microsoft.CodeAnalysis.DiagnosticDescriptor;
+using SemanticModel = Microsoft.CodeAnalysis.SemanticModel;
+
+namespace Analyzers.Analyzers.Parameter;
+
+public abstract class ParameterValueAnalyzer : ParameterAnalyzer
+{
+    protected abstract IValidation Validation { get; }
+
+    protected override void AnalyzeInvocation(
+        OperationAnalysisContext context,
+        InvocationExpressionSyntax syntax,
+        SemanticModel semanticModel
+    )
+    {
+        if (ParameterHasValidValue(syntax, semanticModel, TargetParameterName))
+        {
+            return;
+        }
+
+        context.ReportDiagnostic(Diagnostic.Create(Rule, syntax.Expression.GetLocation()));
+    }
+
+    private bool ParameterHasValidValue(
+        InvocationExpressionSyntax invocationSyntax,
+        SemanticModel semanticModel,
+        string targetParameterName
+    )
+    {
+        ArgumentSyntax? argument = invocationSyntax.ArgumentList.Arguments.FirstOrDefault(
+            argument => GetParameterName(semanticModel, argument).Equals(targetParameterName)
+        );
+
+        if (argument is null)
+        {
+            return true;
+        }
+
+        IOperation? operation = semanticModel.GetOperation(argument.Expression);
+        return operation?.ConstantValue is { HasValue: true, Value: { } value }
+            && Validation.Validate(value);
+    }
+}
